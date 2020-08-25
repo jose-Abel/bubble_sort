076f1a97620701428aa6df42106a19f2c639365a
Yanick <tyankee15@gmail.com>
2020-08-25 13:15:39 -0300

    create bubble sort by

---
+++ bubble_sort_by.rb
@@ -0,0 +1,15 @@
+def bubble_sort_by (array)
+    n = array.length
+    loop do
+        swap = false
+        array.length do |left,right|
+        if left.length <=> right.length 
+            left = right
+        swap = true
+        end
+        # left += 1
+    end
+    array
+end
+end
+puts (["hi","hello","hey"])
