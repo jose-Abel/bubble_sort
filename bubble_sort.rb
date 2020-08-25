beceebf5c3fd6e066e03fe75b813340b21258abd
Yanick <tyankee15@gmail.com>
2020-08-25 18:48:09 -0300

    fixed linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,22 +1,20 @@
 def bubble_sort(arr)
-  a = arr.length()
-  while a > 0
-    noSwaps = true
+  a = arr.length
+  while a.positive?
+    no_swaps = true
     b = 0
     while b < a - 1
       if arr[b] > arr[b + 1]
         arr[b], arr[b + 1] = arr[b + 1], arr[b]
-        noSwaps = false
+        no_swaps = false
       end
       b += 1
     end
-    if noSwaps 
-      break
-    end
+    
+    break if no_swaps
     a -= 1
   end
   arr
 end
-
 puts bubble_sort([100, 243, 210, 120, 40, -80, -300, 2, 4])
 
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,26 +1,25 @@
 def bubble_sort_by(arr)
-
-  a = arr.length()
-  while a > 0
-    noSwaps = true
+  a = arr.length
+  while a.positive?
+    no_swaps = true
     b = 0
     while b < a - 1
       left = arr[b]
       right = arr[b + 1]
-      result = yield(left,right)
-        if result > 0
+      result = yield(left, right)
+        if result.positive?
           arr[b], arr[b + 1] = arr[b + 1], arr[b]
-          noSwaps = false
+          no_swaps = false
         end
       b += 1
     end
-    break if noSwaps
+    break if no_swaps
+
     a -= 1
   end
   puts arr
 end
 
-
-bubble_sort_by(["hi","hello","hey"]) do |left, right|
+bubble_sort_by(%w['hi', 'hello', 'hey']) do |left, right|
   left.length - right.length
 end
